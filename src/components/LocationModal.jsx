742dae7fcefaefd35da9240eb2edbd13d9aa5a91
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-09-12 12:14:30 +0600

    Beautifull back light background designed, button animation set up, modal designed

--- src/components/LocationModal.jsx
+++ src/components/LocationModal.jsx
@@ -1,22 +1,46 @@
-// import { IoMdClose } from "react-icons/io";
-
-{/* Open the modal using document.getElementById('ID').showModal() method */}
-
+import { useState } from "react";
 
 const LocationModal = () => {
+  const [city, setCity] = useState("");
+
+  const handleSubmit = () => {
+    // e.prevenDefault();
+    console.log("Form submited", city);
+  };
+
   return (
+    <dialog id="my_modal_3" className="modal">
+      <div className="modal-box">
+        <h2 className="text-xl font-bold text-gray-800">
+          What is your location?
+        </h2>
+        <form onSubmit={handleSubmit} method="dialog">
+          <div className="flex flex-col justify-center items-center w-full mt-5">
+            <input
+              onChange={(e) => setCity(e.target.value)}
+              type="text"
+              placeholder="Enter City Name"
+              className="w-full border my-3 px-4 py-1 rounded-2xl"
+            />
+            <button className="btn scale-90 text-lg bg-blue-500 shadow-none hover:shadow-lg hover:shadow-blue-400 transition-shadow duration-300 text-gray-200">
+              Get Weather
+            </button>
 
+            <div className="divider divide-neutral">OR</div>
 
-<dialog id="my_modal_3" className="modal">
-  <div className="modal-box">
-    <form method="dialog">
-      {/* if there is a button in form, it will close the modal */}
-      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-    </form>
-    <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click on ✕ button to close</p>
-  </div>
-</dialog>
+            
+            <button className="btn scale-90 text-lg bg-blue-500 shadow-none hover:shadow-lg hover:shadow-blue-400 transition-shadow duration-300 text-gray-200">
+              Use My Location
+            </button>
+          </div>
+
+          {/* if there is a button in form, it will close the modal */}
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+        </form>
+      </div>
+    </dialog>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,18 @@ import LocationModal from "../components/LocationModal";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center  ">
+    <div className="flex flex-col items-center justify-center  bg-radial-[at_50%_50%] from-blue-200  to-transparent to-50%">
       <h2 className="text-2xl text-center md:text-3xl lg:text-4xl text-blue-300 font-bold md:font-extrabold">
         Climate <span className="text-blue-400 ">Pulse</span>
       </h2>
-      <p className="text-md text-gray-300 my-3">
+      <p className="text-md text-gray-400 my-3">
         Check weather today in Climate Pulse
       </p>
 
       {/* Button and Modal */}
       <div className="flex justify-center">
-        {/* <button
-          popoverTarget="my-modal-1"
-          className="btn bg-blue-500 hover:bg-blue-600 transition-colors duration-100 text-gray-200"
-        >
-          Check Weather
-        </button> */}
         <button
-          className="btn bg-blue-500 hover:bg-blue-600 transition-colors duration-100 text-gray-200"
+          className="btn scale-90 text-lg bg-blue-500 shadow-none hover:shadow-lg hover:shadow-blue-400 transition-shadow duration-300 text-gray-200"
           onClick={() => document.getElementById("my_modal_3").showModal()}
         >
           Check Weather
